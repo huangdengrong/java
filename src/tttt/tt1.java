2de0a130afa52c368eeceb1b42e1cdcdfd75a69b
huangdengrong <858867707@qq.com>
2018-05-09 14:01:57 +0800

    test

---
+++ src/tttt/tt1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package tttt;
+
+/**
+ * @author Administrator
+ *
+ */
+public class tt1 {
+	public void fun() {
+		System.out.print("我们的祖国");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
